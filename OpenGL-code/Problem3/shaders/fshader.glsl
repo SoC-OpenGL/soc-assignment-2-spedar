cd4ed21631e5b661c3329b3499f020704c3620f2
spedar <deep.satra@yahoo.in>
2020-04-20 10:57:15 +0530

    Completed Problems

--- OpenGL-code/Problem3/shaders/fshader.glsl
+++ OpenGL-code/Problem3/shaders/fshader.glsl
@@ -150,59 +150,7 @@ vec2 ParallaxMapping(vec2 texCoords, vec3 viewDir)
 //-----------------------------------------------------------------------------------
 
 void main(){
-/*
-    int cutOffState = 0;
-    float clampIntensity;
-    vec3 base = pow(texture(material.ALBEDO, TexCoord).rgb, vec3(2.2));
-    vec3 normal_n = getNormalFromMap();
-   
-    vec3 ambient = globalAmbient * base * texture(material.AMBIENT, TexCoord).r;
 
-    vec3 lightDir;
-    if (light.type == 0) // point light
-        lightDir = normalize(light.position - FragPos);   // viewloc - ...
-    else if (light.type == 1){
-        lightDir = -light.direction;
-    }
-    else{
-        lightDir = normalize(light.position - FragPos);
-        float theta = dot(lightDir, normalize(-light.direction));
-        float epsilon   = light.cutOff - light.outerCutOff;
-        clampIntensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);    
-        if (theta < light.cutOff)
-            cutOffState = 1;
-    }
-
-
-    vec3 reflectDir = reflect(-lightDir, normal_n);
-    vec3 viewDir = normalize(viewLoc - FragPos);
-    vec3 halfway = normalize(lightDir + viewDir);
-    float spec;
-    if (blinnPhong != 0)
-        spec = pow(max(dot(normal_n, halfway), 0.0), material.shininess*(texture(material.ROUGHNESS, TexCoord).r));
-    else
-        spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess*(texture(material.ROUGHNESS, TexCoord).r));
-    vec3 specular =  spec * material.specular * light.specular;
-    float diffuseStrength = max(dot(lightDir, normal_n), 0.0);
-    vec3 diffuse = diffuseStrength * base * light.diffuse;
-
-    if (light.type == 0){
-        float distance = length(light.position - FragPos);
-        float attenuation = 1.0 / (light.constant + light.linear * distance + 
-                        light.quadratic * (distance * distance));  
-        specular *= attenuation;
-        diffuse *= attenuation;
-    }
-
-    vec3 Color =  ambient + diffuse + specular;
-    if (cutOffState==1){
-        Color = Color - diffuse - specular;
-        diffuse *= clampIntensity; specular *= clampIntensity;
-        Color = Color + diffuse + specular;
-    }
-
-    frag_colour = vec4(Color, 1.0f);
-*/
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
     if(light.type == 1){
